2572b97c50315ac08fdf621bd4ba14486c8914d7
Vivek Kumar <vivekkumar14728@gmail.com>
2022-05-29 11:34:33 +0530

    Initial Commit

---
+++ pattern/pattern1.java
@@ -0,0 +1,20 @@
+package pattern;
+
+/*
+ 1234
+ 1234
+ 1234
+ 1234  
+*/
+public class pattern1 {
+	
+	public static void main(String[]args) {
+	
+		for(int i=0;i<4;i++) {
+			for(int j=1;j<5;j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
